d73822fea9fb04388a9a002e21eeb2690aac7277
Pedro Martins <pedro.miguel.martins9@gmail.com>
2022-12-03 01:04:48 +0000

    Added alternative solution

---
+++ 02/alternate.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+static map<string, int> pt1LUT = {
+    {"A X", 1 + 3},
+    {"A Y", 2 + 6},
+    {"A Z", 3 + 0},
+    {"B X", 1 + 0},
+    {"B Y", 2 + 3},
+    {"B Z", 3 + 6},
+    {"C X", 1 + 6},
+    {"C Y", 2 + 0},
+    {"C Z", 3 + 3},
+};
+
+static map<string, int> pt2LUT = {
+    {"A X", 0 + 3},
+    {"A Y", 3 + 1},
+    {"A Z", 6 + 2},
+    {"B X", 0 + 1},
+    {"B Y", 3 + 2},
+    {"B Z", 6 + 3},
+    {"C X", 0 + 2},
+    {"C Y", 3 + 3},
+    {"C Z", 6 + 1},
+};
+
+int main()
+{
+    string line;
+    ifstream input_file("input.txt");
+
+    if (!input_file.is_open())
+        return 1;
+
+    int score_pt1 = 0;
+    int score_pt2 = 0;
+
+    while (getline(input_file, line, '\n'))
+    {
+        int left = line[0] - 'A' + 1;
+        int right = line[2] - 'X' + 1;
+        score_pt1 += ((4 + (right - left)) % 3) * 3 + right;
+        score_pt2 += ((left + right) % 3) + 1 + ((right - 1) * 3);
+    }
+    cout << "Total Score pt1: " << score_pt1 << endl;
+    cout << "Total Score pt2: " << score_pt2 << endl;
+
+    return 0;
+}
